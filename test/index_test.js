d51a8bcb4d7110b5b6978109ca0c8ab9b3e0e912
Satvir Sandhu <356Sandhu@gmail.com>
2020-03-24 14:54:16 -0400

    add more test cases for add function

--- test/index_test.js
+++ test/index_test.js
@@ -6,12 +6,22 @@ describe("Math", () => {
     it("3 + 3 returns 6", () => {
       assert.equal(6, MathStuff.add(3, 3));
     });
+
+    it("-3 + 3 returns 0", () => {
+      assert.equal(0, MathStuff.add(-3, 3));
+    });
+
+    it("9999 + 1 returns 10000", () => {
+      assert.equal(10000, MathStuff.add(9999, 1));
+    });
   });
+
   describe(".subtract()", () => {
     it("5 - 2 returns 3", () => {
       assert.equal(3, MathStuff.subtract(5, 2));
     });
   });
+
   describe(".multiply()", () => {
     it("2 * 2 returns 4", () => {
       assert.equal(4, MathStuff.multiply(2, 2));
