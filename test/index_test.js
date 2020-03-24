2f9139f8e351073a5c49960b74a8c5ca92e1734e
Satvir Sandhu <356Sandhu@gmail.com>
2020-03-24 13:59:17 -0400

    add subtraction test

--- test/index_test.js
+++ test/index_test.js
@@ -7,6 +7,11 @@ describe("Math", () => {
       assert.equal(6, MathStuff.add(3, 3));
     });
   });
+  describe(".subtract()", () => {
+    it("5 - 2 returns 3", () => {
+      assert.equal(3, MathStuff.subtract(5, 2));
+    });
+  });
   describe(".multiply()", () => {
     it("2 * 2 returns 4", () => {
       assert.equal(4, MathStuff.multiply(2, 2));
