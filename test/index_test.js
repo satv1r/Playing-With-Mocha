89863c46b18cc325a18c24bd56ad639d743728d8
Satvir Sandhu <356Sandhu@gmail.com>
2020-03-24 13:54:12 -0400

    add .add() test case

--- test/index_test.js
+++ test/index_test.js
@@ -2,6 +2,11 @@ const assert = require("assert");
 const MathStuff = require("../MathStuff.js");
 
 describe("Math", () => {
+  describe(".add()", () => {
+    it("3 + 3 returns 6", () => {
+      assert.equal(6, MathStuff.add(3, 3));
+    });
+  });
   describe(".multiply()", () => {
     it("2 * 2 returns 4", () => {
       assert.equal(4, MathStuff.multiply(2, 2));
